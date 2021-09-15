47d68ddefda645b193550f4b936d6127108860c9
Dembuxxxx <dems75018@hotmail.fr>
2021-09-16 01:17:03 +0200

    base sql

---
+++ eval.sql
@@ -0,0 +1,11 @@
+-- connexion au serveur MySQL
+mysql -h localhost:3306 -u root -p root
+
+-- Afficher la liste des bases de données
+SHOW databases;
+
+-- Création de la  base de données
+CREATE DATABASE  resa_place_cinema ;
+
+-- Sélectionner la bbd souhaité pour la création de table
+USE resa_place_cinema ;
